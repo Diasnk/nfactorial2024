943e031699c0cc2c5ef3f74f37674040f600d31e
Diasnk <diasnugumanov258@gmail.com>
2024-06-06 09:26:31 +0500

    post id

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -6,16 +6,16 @@ export default function Blog({params}:any){
         <div className='w-full flex justify-center'>
             <div className='w-1/2 md:w-1/3 border-2 rounded-lg'>
                 <h1 className='font-semibold text-md m-1'>
-                    {data[params.post].title}
+                    {data[params.post -1].title}
                 </h1>
                 <p className='pt-4 m-1'>
-                    {data[params.post].description}
+                    {data[params.post -1].description}
                 </p>
                 <h4 className='font-semibold m-1 '>
-                    By: {data[params.post].author}
+                    By: {data[params.post -1].author}
                 </h4>
                 <p className='text-sm m-1'>
-                    {data[params.post].date}
+                    {data[params.post -1].date}
                 </p>
                 <Link href="/blog">
                     <button className="border-2 m-1 bg-slate-200 rounded-lg p-0.5">
