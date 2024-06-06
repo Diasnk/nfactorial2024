185dc813ebf0886ca5566a1f86f2350f1658d7c6
Diasnk <diasnugumanov258@gmail.com>
2024-06-06 07:33:49 +0500

    slugs

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -1,11 +1,28 @@
+import {data} from '@/app/data/data.js'
+import Link from 'next/link'
 
-
-export default function ShowBlog(){
+export default function Blog({params}:any){
     return(
-        <div className="w-48 h-60 bg-[#6C63FF] rounded-xl hover:shadow-2xl">
-            <h1 className="font-bold text-lg ">
-                
-            </h1>
+        <div className='w-full flex justify-center'>
+            <div className='w-1/2 md:w-1/3 border-2 rounded-lg'>
+                <h1 className='font-semibold text-md m-1'>
+                    {data[params.post].title}
+                </h1>
+                <p className='pt-4 m-1'>
+                    {data[params.post].description}
+                </p>
+                <h4 className='font-semibold m-1 '>
+                    By: {data[params.post].author}
+                </h4>
+                <p className='text-sm m-1'>
+                    {data[params.post].date}
+                </p>
+                <Link href="/blog">
+                    <button className="border-2 m-1 bg-slate-200 rounded-lg p-0.5">
+                        Return back
+                    </button>
+                </Link>
+            </div>
         </div>
     )
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,20 +2,25 @@ import Link from "next/link";
 import {data} from '@/app/data/data.js'
 import Post from "@/components/post";
 
-export default function Blog(){
+export default function BlogList(){
     
     return(
-        <div>
-            {/* {
+        <div className="w-full pl-2 pt-4 flex flex-col justify-center">
+            {
                 data.map((name, index) => (
                     <>
-                        <div key={index} >
-                            {name.id}
+                        <div className="font-semibold text-xl space-y-2 p-2 w-1/4" key={index} >
+                            Made by: {name.author} <br/>
+                            Date: {name.date} <br/>
+                            <Link href={"/blog/" + name.id}>
+                                <button className="border-2 bg-slate-200 rounded-lg p-0.5">
+                                    View
+                                </button>
+                            </Link>
                         </div>
                     </>
-            ))} */}
+            ))}
 
-            <Post/>
 
         </div>
     )
